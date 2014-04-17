88e4d886dcb6af533f5b12e498940a0ab3156141
PJ O Halloran <pjohalloran@gmail.com>
2014-04-17 21:36:58 +0100

    Level parsing wip.

---
+++ Assets/Scripts/Game/LevelReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+using BalloonBasket.Tech;
+
+public class LevelReader : MonoBehaviour {
+
+	public void ParseLevel(string filename) {
+		TextAsset asset = Utils.LoadResource (filename) as TextAsset;
+		foreach (char c in asset.text) {
+
+		}
+	}
+}
