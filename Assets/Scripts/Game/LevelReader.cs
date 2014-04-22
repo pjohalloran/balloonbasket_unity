8f070db7431e1d363c41a6fcde9f5331862c7f6c
pjohalloran <pjohalloran@gmail.com>
2014-04-22 23:22:54 +0100

    Fix level parsing bugs.

--- Assets/Scripts/Game/LevelReader.cs
+++ Assets/Scripts/Game/LevelReader.cs
@@ -18,48 +18,23 @@ public class LevelReader : MonoBehaviour {
 
 	public static List<List<char>> ParseLevel(string filename) {
 		TextAsset asset = Utils.LoadResource (filename) as TextAsset;
-
 		List<List<char>> data = new List<List<char>>();
-
 		int rI = 0;
-		int cI = 0;
 
 		data.Add(new List<char>());
 
 		bool skipChar = false;
 		foreach (char c in asset.text) {
 			if(c == '\n') {
-				Debug.Log ("New Line");
 				data.Add(new List<char>());
 				rI++;
-				cI = 0;
-				//skipChar = true;
-			} else if(c == LevelReader.O_BALLOON_START) {
-				Debug.Log (string.Format("Balloon Start at {0},{1}", rI, cI));
-			} else if(c == LevelReader.O_EMPTY_SPACE) {
-				Debug.Log (string.Format("Empty at {0},{1}", rI, cI));
-			} else if(c == LevelReader.O_MINE) {
-				Debug.Log (string.Format("Mine at {0},{1}", rI, cI));
-			} else if(c == LevelReader.O_SEAGULL) {
-				Debug.Log (string.Format("Seagull at {0},{1}", rI, cI));
-			} else if(c == LevelReader.O_TREETOP) {
-				Debug.Log (string.Format("Treetop at {0},{1}", rI, cI));
-			} else if(c == LevelReader.O_TREETOPTALL) {
-				Debug.Log (string.Format("TreetopTall at {0},{1}", rI, cI));
-			} else if(c == LevelReader.O_WIND150) {
-				Debug.Log (string.Format("Wind Small at {0},{1}", rI, cI));
-			} else if(c == LevelReader.O_WIND200) {
-				Debug.Log (string.Format("Wind Large at {0},{1}", rI, cI));
-			} else if(c == LevelReader.O_FINISH) {
-				Debug.Log (string.Format("Finish at {0},{1}", rI, cI));
-			} else {
-				Debug.Log("Unknown char: " + c);
+				skipChar = true;
 			}
-			//if(skipChar) {
+			if(!skipChar) {
 				data[rI].Add(c);
+			} else {
 				skipChar = false;
-			//}
-			cI++;
+			}
 		}
 
 		return data;
