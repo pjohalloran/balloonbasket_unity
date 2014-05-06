04bf955708c88805efe0babccafd21602dfc326c
PJ O Halloran <pjohalloran@gmail.com>
2014-05-06 22:02:31 +0100

    Build helper PSM scripts.

---
+++ BuildScripts/PsmDebugListDevices.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\SCE\UnityForPSM\tools\PsmDevice\PsmDeviceForUnity.exe" -list_devices
+pause
---
+++ BuildScripts/PsmDebugShowDeviceLog.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\SCE\UnityForPSM\tools\PsmDevice\PsmDeviceForUnity.exe" -get_log bd68967e-f968-4d7d-ba53-61d737890fd8
+pause
